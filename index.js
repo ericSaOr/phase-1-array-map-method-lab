b277114c37a96f5b54744a2d92e6d0c1bf9c6d0d
Eric Sanchez <ericfsod@gmail.com>
2021-07-05 17:00:35 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(elements => elements.split(' ').map(string => string.charAt(0).toUpperCase(0) + string.substring(1)).join(' '))
 }
